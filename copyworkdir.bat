18ef44dc5024c46a71360fc3058a98f29ef39dc3
sualfred <su4lfred@gmail.com>
2016-11-26 09:35:31 +0100

    update

--- copyworkdir.bat
+++ copyworkdir.bat
@@ -1,7 +1,16 @@
 @echo off
 set ftvkrypton=E:\github\skin.fTVfred-krypton
+set target=skin.fTVfred-krypton
+set htpc=\\NUCFRED\Kodi\portable_data\addons\skin.fTVfred-krypton
 echo. 
 echo Copying files
 echo. 
-XCOPY %ftvkrypton% skin.fTVfred-krypton /E /C /Q /I /Y
+XCOPY %ftvkrypton% %target% /E /C /Q /I /Y
+del /q /s %target%\*.pyo 
+del /q /s %target%\*.pyc 
+del /q /s %target%\*.psd 
+rd /S /Q %target%\.git
+XCOPY %target% %htpc% /E /C /Q /I /Y
+
+
 pause
